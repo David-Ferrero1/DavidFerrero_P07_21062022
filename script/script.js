0ff5dffbca5109f42196f99c65ef772ca7fbcc05
David Ferrero <ferrero_d@hotmail.com>
2022-07-13 12:28:04 +0200

    fix boucle for V2

--- script/script.js
+++ script/script.js
@@ -5,15 +5,23 @@ async function filterRecettes() {
     recipes = await fetchSearch()
     let recettes = []
 
-    for(let i = 0; i< recipes.length; i++){
-        if(recipes[i].name.toLowerCase().includes(searchTerm) || recipes[i].description.toLowerCase().includes(searchTerm)){
-          recettes.push(recipes[i]);
-          for(let y = 0; y< recipes.length ; y++){
-            for(let z = 0; z<recipes[i].ingredients.length; z++){
-              if(recipes[i].ingredients[z].ingredient.toLowerCase().includes(searchTerm)){
-              }
+    for (let i = 0; i < recipes.length; i++) {
+        if (
+            recipes[i].name.toLowerCase().includes(searchTerm) ||
+            recipes[i].description.toLowerCase().includes(searchTerm)
+        ) {
+            recettes.push(recipes[i])
+
+            for (let y = 0; y < recipes.length; y++) {
+                for (let z = 0; z < recipes[i].ingredients.length; z++) {
+                    if (
+                        recipes[i].ingredients[z].ingredient.toLowerCase() ===
+                        searchTerm
+                    ) {
+                        recettes.concat(recipes[i])
+                    }
+                }
             }
-           }
         }
     }
     if (appareilsTerm.length > 0) {
